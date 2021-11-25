4e45a9054556c3ea92b15571c4314364a9bca8c9
weeraphon <60165647+62130500131@users.noreply.github.com>
2021-11-25 21:10:23 +0700

    Add files via upload

---
+++ src/main/java/sit/integrated/project/controllers/UsersController.java
@@ -0,0 +1,103 @@
+package sit.integrated.project.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+import sit.integrated.project.exceptions.ExceptionResponse;
+import sit.integrated.project.exceptions.ProductsException;
+import sit.integrated.project.models.Feedback;
+import sit.integrated.project.models.JwtUser;
+import sit.integrated.project.models.Products;
+import sit.integrated.project.models.Users;
+import sit.integrated.project.repositories.JwtUserRepositories;
+import sit.integrated.project.repositories.UsersRepositories;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/Users")
+@CrossOrigin
+public class UsersController {
+    @Autowired
+    private UsersRepositories usersRepositories;
+
+    @Autowired
+    private JwtUserRepositories jwtusersRepositories;
+
+    private BCryptPasswordEncoder bcrypt = new BCryptPasswordEncoder();
+
+    @GetMapping("/GetUsers")
+    public List<Users> ListAllUsers(){
+        return usersRepositories.findAll();
+    }
+
+    @GetMapping("/GetUsers/{id}")
+    public Users getUserById(@PathVariable int id){
+    return usersRepositories.findById(id).orElse(null);
+
+    }
+
+
+    @PostMapping("/Login")
+    public Users login(@RequestBody JwtUser user){
+        List<Users>  usersList = usersRepositories.findAll();
+        Users[] userArray = new Users[usersList.size()];
+        usersList.toArray(userArray);
+        String checkUser;
+        String checkPass;
+        for(int i = 0 ; i < userArray.length; i++ ){
+            checkUser = userArray[i].getUserName();
+            checkPass = userArray[i].getUserPassword();
+            if(checkUser.contains(user.getUsername())  && checkPass.contains(user.getPassword()) ){
+                return userArray[i];
+            }
+        }
+        return null;
+    }
+
+
+    @PostMapping("/Create")
+    public Users createUsers(@RequestBody Users user){
+            String username = user.getUserName();
+            String password = bcrypt.encode(user.getUserPassword());
+            JwtUser userJwt = new JwtUser();
+            userJwt.setUsername(username);
+            userJwt.setPassword(password);
+            jwtusersRepositories.save(userJwt);
+            List<Users> listuser = usersRepositories.findAll();
+            Users[] userArrays = new Users[listuser.size()];
+            listuser.toArray(userArrays);
+            user.setUserId(usersRepositories.userLatestId() + 1);
+            usersRepositories.save(user);
+            return user;
+    }
+
+
+
+    @PutMapping("/Edit/{id}")
+    public Users editUser(@RequestBody Users users, @PathVariable int id){
+
+            if (hasFoundId(id) && (id == users.getUserId())) {
+                usersRepositories.save(users);
+                return users;
+            } else throw new ProductsException(ExceptionResponse.ERROR_CODE.ITEM_DOES_NOT_EXIST, "DOES NOT EXIST");
+    }
+
+    @DeleteMapping("/Delete/{id}")
+    public String deleteUsers(@PathVariable int id){
+            usersRepositories.deleteById(id);
+            String s = String.valueOf(id);
+            return s + "has been deleted";
+    }
+
+    public boolean hasFoundId(int id){
+        List<Users> users = usersRepositories.findAll();
+        for (int i = 0; i < users.size(); i++) {
+            if(users.get(i).getUserId() == id){
+                return true;
+            }
+        }
+        return false;
+    }
+}
